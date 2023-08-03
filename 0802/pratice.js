6276a6bf49b99807ec1c4adf943076127172021b
haileyja <somdari0423@gmail.com>
2023-08-03 09:06:07 +0900

    JS2

---
+++ 0802/pratice.js
@@ -0,0 +1,33 @@
+let string1 = 'long length'
+
+console.log(string1)
+console.log(string1)
+console.log(string1)
+console.log(string1)
+console.log(string1)
+
+
+// 변수 값 바꾸기  1
+let a = 1
+let b = 2
+
+a = 2
+b = 1
+console.log(a)
+console.log(b)
+
+// 변수 값 바꾸기  2
+let c = 5
+let d = 3
+let temp = c;
+c = d
+d = temp
+
+
+let value = 'apple';
+let condition = true;
+if (condition) {
+    value = 'banana'
+}
+
+console.log(value)
